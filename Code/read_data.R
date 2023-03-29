3d10dcaca1b5752445c17f1823a4faa2d75534b5
Charlotte <crf7@leicester.ac.uk>
2023-03-29 14:55:23 +0100

    Create a read_data script

---
+++ Code/read_data.R
@@ -0,0 +1,8 @@
+## RAGE: Regression after Gene Expression
+
+### Read in data from raw datafile csv and save in rds format
+
+library(tidyverse)
+library(fs)
+###fs package - function for writing down paths and files - does file and directory handling
+
--- README.md
+++ README.md
@@ -2,6 +2,7 @@ RAGE README
 
 Project started 29/03/23
 
-TEST
+## Exercise for the Git Training Course
 
-TEST2
+Create a read_data R script to read the raw data into a cache file
+
